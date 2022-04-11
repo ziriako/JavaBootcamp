6e150813b77e757396710a850d50caa9c1618347
Ziriako <ziriako@gmail.com>
2022-04-11 18:34:24 -0500

    Ejercicio Sesion 6

---
+++ Restaurante/src/com/qaminds/restaurante/Main.java
@@ -0,0 +1,11 @@
+package com.qaminds.restaurante;
+
+public class Main {
+    public static void main(String args[]){
+        Mesero primerMesero = new Mesero();
+        primerMesero.traerMenu();
+
+        System.out.println("Inicial");
+
+    }
+}
---
+++ Restaurante/src/com/qaminds/restaurante/Mesero.java
@@ -0,0 +1,9 @@
+package com.qaminds.restaurante;
+
+public class Mesero {
+    String name;
+
+    public void traerMenu(){
+        System.out.println("Trayendo menu");
+    }
+}
